564b8e485a568e9de73889525262bfa918034143
Lenora Porter <lporter630@gmail.com>
2024-05-28 09:55:21 -0400

    Correct layout width

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,7 +19,53 @@ const Layout = ({ children }) => {
 	return (
 		<div className={`min-h-screen flex flex-col ${isDarkMode ? "dark" : ""}`}>
 			<header className="bg-gray-900 text-white p-4 flex justify-between items-center">
-				<h1 className="text-3xl">My Documentation Site</h1>
+				<div className="px-6">
+					<svg
+						width="32"
+						height="32"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 90 90"
+						tabindex="-1"
+						class="DigitalOceanSmileyStyles__StyledSvg-sc-64ca7b97-4 fOCpep"
+					>
+						<circle
+							cx="45"
+							cy="45"
+							r="36.25"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="17.5"
+							stroke-dasharray="170.824100538945 56.941366846315"
+							stroke-dashoffset="113.88273369263"
+							class="DigitalOceanSmileyStyles__StyledCircle-sc-64ca7b97-0 GowsZ"
+						></circle>
+						<rect
+							x="3.25"
+							y="61.5"
+							width="11"
+							height="11"
+							fill="currentColor"
+							class="DigitalOceanSmileyStyles__StyledPixelSm-sc-64ca7b97-1 dMNrpE"
+						></rect>
+						<rect
+							x="14.25"
+							y="72.5"
+							width="13.5"
+							height="13.5"
+							fill="currentColor"
+							class="DigitalOceanSmileyStyles__StyledPixelMd-sc-64ca7b97-2 eZYiSP"
+						></rect>
+						<rect
+							x="27.5"
+							y="55.25"
+							width="17.25"
+							height="17.25"
+							fill="currentColor"
+							class="DigitalOceanSmileyStyles__StyledPixelLg-sc-64ca7b97-3 grYxqE"
+						></rect>
+					</svg>
+				</div>
 				<DarkModeToggle
 					isDarkMode={isDarkMode}
 					toggleDarkMode={toggleDarkMode}
